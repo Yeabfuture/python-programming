e9bfcc12fe5502537b25d078cff184d57221a591
Yeabsira <yeabsiramersha58@yeabfuture.com>
2023-10-29 12:14:55 +0300

    show me something unique

--- calculating_age.py
+++ calculating_age.py
@@ -1,1 +1,3 @@
 year_of_birth = input("Year of birth: ")
+age = 2023 - int(year_of_birth)
+print(age)
---
+++ pound_kg.py
@@ -0,0 +1,3 @@
+weight_in_pound = input('Enter your weright in pound : ')
+weight_in_kg = 0.453 * float(weight_in_pound)
+print(weight_in_kg)
