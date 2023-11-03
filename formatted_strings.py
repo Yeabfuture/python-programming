0fd030db5615ad24bcf39c38730cf2b3880f5c10
Yeabsira <yeabsiramersha58@yeabfuture.com>
2023-11-03 10:24:36 +0300

    formatted strings

---
+++ formatted_strings.py
@@ -0,0 +1,8 @@
+# useful when we want to generate a dynamic text
+first = 'Jhon'
+second = 'Smith'
+
+message = f'{first} {second} is a brillient gentle man!'
+print(message)
+
+# the curly braces in the formatted strings serve as a method that be used to put a dynamic text 
