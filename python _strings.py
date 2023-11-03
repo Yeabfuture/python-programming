4c8986d0321ae82fae3f6181ab613e38210efe85
Yeabsira <yeabsiramersha58@yeabfuture.com>
2023-11-03 10:18:47 +0300

    My commit

---
+++ python _strings.py
@@ -0,0 +1,22 @@
+# we can use both single and double qoutes
+
+# We can use ''' ''' or triple qoutes for a multi line strings
+
+''' email =  Hello Dear Yeabsira,
+
+This is from the support team and we just wanted to say Happy Birth Day
+
+Best
+ 
+
+print(email)  '''
+
+text = "Hello Yeabsira"
+print(text[0:3])
+
+# we can access the index of a text using name[index]
+# when we index multile characters instd of one we can use [x:y] and it will print all the characters from x to y excluding y
+# If we dont supply the end index it will take the end of the string as the end index and similar t0 this if we dont supply the first indext it will take 0 as the first and index
+
+name = 'Jennifer'
+print(name[1:-1])
