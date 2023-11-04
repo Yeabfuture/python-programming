d1338f3176f6f04d4503f8d31b30b80bd96b93e3
Yeabsira <yeabsiramersha58@yeabfuture.com>
2023-11-04 06:52:37 +0300

    operators commit

--- operetors.py
+++ operetors.py
@@ -16,11 +16,25 @@ if has_high_income or has_good_credit:
 
 #Comparsion Operator
 
-temp = int(input("What is the temp "))
+ temp = int(input("What is the temp "))
 
 if temp > 30:
     print("Its a hot day")
 elif temp < 10:
     print("Its a cold day")
 else:
-    print("Its a lovely day")
+    print("Its a lovely day") 
+
+
+# Project about name validator
+
+name = input("What is your name?")
+leng = len(name)
+
+if leng < 3:
+    print("Name must be atleast 3 characters")
+elif leng >50:
+    print("Name can be max of 50 characters")
+else:
+    print("Name looks good!")
+
