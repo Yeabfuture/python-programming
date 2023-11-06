76c001c523e6eca32940010757d7a12cd6f19ff8
Yeabsira <yeabsiramersha58@yeabfuture.com>
2023-11-06 07:02:53 +0300

    monday morning

---
+++ quiz_game.py
@@ -0,0 +1,7 @@
+playing = input(" do you want to play my game? 'y' for yes and 'n' for no: " )
+
+if playing.lower == 'y':
+    print("Welcome to my game!")
+else:
+    print("Good bye loser!")
+    exit()
---
+++ while_loops.py
@@ -0,0 +1,9 @@
+i = 1
+
+while i<= 5:
+    print(i * '*')
+    i+=1
+
+print("Done")
+
+# While loops are useful when it comes to conditions where we don't exactly know the number of times a loops has to be excuted.
