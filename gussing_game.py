bd331a7c42a589e64e1af29c5e400dd885e54bdf
Yeabsira <yeabsiramersha58@yeabfuture.com>
2023-11-06 07:16:29 +0300

    else update

--- gussing_game.py
+++ gussing_game.py
@@ -1,12 +1,10 @@
 number_of_trials = 0
 lucky_number = 9
-
-
 while number_of_trials < 4:
     user_guess = int(input("Guess: "))
     if user_guess == lucky_number:
         print("You win!")
-        quit()
-
+        break
     number_of_trials += 1
-print("Sorry you failed!")
+else:
+    print("Sorry you failed!")
