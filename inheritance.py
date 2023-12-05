bc7cb3bb0b526c5ded98da425619f9230e7423c4
Yeabsira <yeabsiramersha58@yeabfuture.com>
2023-12-05 21:17:44 +0300

    inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,3 +1,5 @@
+# Inheritance help us avoid reputation in our code by helping us to reuse methods and attributes form a parent class in our child class.
+
 class Mammal:
     def walk():
         print("walk")
